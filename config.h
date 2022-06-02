e69c5bc65d659065805f56c172dc474d341102e7
luis96raul1 <luis96raul1@gmail.com>
2022-06-02 13:20:24 -0500

    changed path to glcdfont

--- config.h
+++ config.h
@@ -20,7 +20,8 @@
 //#define RETRO_TAPPING
 #define IGNORE_MOD_TAP_INTERRUPT
 
-#define OLED_FONT_H "keyboards/crkbd/keymaps/soundmonster/glcdfont.c"
+#define OLED_FONT_H "keyboards/crkbd/keymaps/myCorneKeyboardLayout/glcdfont.c"
+
 // #define OLED_FONT_WIDTH 5
 // #define OLED_FONT_HEIGHT 7
 
